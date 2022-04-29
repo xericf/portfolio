57be04351a790f5529188c5193d344f1e73e3621
xericf <xericf@gmail.com>
2022-04-29 02:45:25 -0600

    typo

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://unpkg.com/browse/three@0.139.2/build/three.js';
-import { OrbitControls } from 'https://unpkg.com/browse/three@0.139.2/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.139.2/build/three.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.139.2/examples/jsm/controls/OrbitControls.js';
 
 // initial three.js setup
 const scene = new THREE.Scene();
