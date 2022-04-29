454a582ca6d6aba32b87167681ca4302f668390a
xericf <xericf@gmail.com>
2022-04-29 02:16:20 -0600

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from './OrbitControls.js';
 
 // initial three.js setup
 const scene = new THREE.Scene();
