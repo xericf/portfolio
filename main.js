1c951a3b4325c7c25c83478f6f3c98092ac78056
xericf <xericf@gmail.com>
2022-04-29 02:56:37 -0600

    build 1 vite

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.139.2/build/three.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.139.2/examples/jsm/controls/OrbitControls.js';
-
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // initial three.js setup
 const scene = new THREE.Scene();
 
